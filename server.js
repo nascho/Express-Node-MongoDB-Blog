716eb18e70517210b88cbddb355c7c2815d90c56
Nas <n.choudhury1@live.co.uk>
2024-02-03 23:19:22 +0000

    updated basic routing

--- server.js
+++ server.js
@@ -1,8 +1,52 @@
 import http from 'http';
+import fs from 'fs';
 
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    // res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
+
+    // basic routing
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        // redirect    
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html'
+            res.statusCode = 404;
+            break;
+    }
+
+    // res.write('bonjour, mr not-console');
+    // res.write('<h1>bonjour, mr not-console</h1>');
+    // res.end();
+
+    // sending html file request
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+
 });
 
 server.listen(5999, 'localhost', () => {
